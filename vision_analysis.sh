aac4012118c5a7b69e1dedfdb1ebbde7f558b17d
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-07-20 15:49:52 -0400

    shellanalysis

---
+++ vision_analysis.sh
@@ -0,0 +1,4 @@
+./header.py test1/ ../../raw_data/coordinates_gene_counts_flow_cytometry.txt mouse
+./gold_standard.py test1/ /Users/boris/Downloads/mouse/mouse.source mouse
+./deviation_matrix.py test1/
+./threshold_analysis.py test1/
