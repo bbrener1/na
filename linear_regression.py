75be2d73480a435897ef107b709af5534f810052
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-08-02 22:24:21 -0400

    linear reg

--- linear_regression.py
+++ linear_regression.py
@@ -106,96 +106,45 @@ def predict(data, true_values, slopes, intercepts, means, correlation):
 
     zero_mask = data != 0
 
+    correlation = np.copy(correlation)
     correlation[zero_mask] = 0
-
-
-    unweighted = np.multiply(np.tile(data,(slopes.shape[0],1)).T,slopes) + intercepts
-
-    weighted = np.multiply(unweighted,np.abs(correlation))
-
-    # prediction1 = np.mean(unweighted,axis=0)
-
-    prediction1 = np.divide(np.sum(weighted,axis=0),np.sum(np.abs(correlation),axis=0))
-
-
-
-    # adj_slopes = np.multiply(slopes,correlation)
-    # adj_slopes[np.identity(slopes.shape[0],dtype=bool)] = 0
-    #
-    # intercepts[np.logical_not(zero_mask)] = 0
-    # weighted_intercepts = np.multiply(intercepts, correlation)
-    #
-    # weights = np.sum(correlation[zero_mask],axis = 0)
     #
     #
-    # # print "Data shape"
-    # # print data.shape
-    # # print np.dot(data,slopes).shape
-    # #
-    # prediction1 = np.divide((np.dot(data,adj_slopes) + np.sum(weighted_intercepts,axis=0)),weights)
+    # unweighted = np.multiply(np.tile(data,(slopes.shape[0],1)).T,slopes) + intercepts
     #
-    # print prediction1.shape
-    # print true_values.shape
-
-    print "Prediction 1"
-    print pearsonr(prediction1, true_values)
-
-
-
-    # prediction2 = np.dot(data[zero_mask],adj_slopes[zero_mask].T[zero_mask].T)
+    # weighted = np.multiply(unweighted,np.abs(correlation))
     #
-    # prediction2 = np.divide(prediction2,weights[zero_mask]) + intercepts[index][zero_mask]
+    # # prediction1 = np.mean(unweighted,axis=0)
     #
-    # print prediction2.shape
-    # print prediction2[:10]
-    # print intercepts[index][np.logical_not(zero_mask)].shape
-    # print weights[np.logical_not(zero_mask)].shape
-
-    # print prediction2.shape
-    # print prediction2[:10]
-    # print true_values[np.logical_not(zero_mask)][:10]
-
+    # prediction1 = np.divide(np.sum(weighted,axis=0),np.sum(np.abs(correlation),axis=0))
+    #
+    #
+    #
+    # print "Prediction 1"
+    # print pearsonr(prediction1, true_values)
+    #
     # print "Prediction 2"
-    # print pearsonr(prediction2, true_values[zero_mask])
+    print pearsonr(data, means)
 
-    # part_slopes = np.multiply(slopes,partial)
-    # part_weights = np.mean(part_slopes,axis=0)
-    #
-    # prediction2a = np.dot(data[zero_mask],part_slopes.T[targets].T)
-    # prediction2a = np.divide(prediction2,part_weights[targets]) + intercepts[index][targets]
-    # weights = np.ones(weights.shape)
-    #
-    # prediction2a = np.dot(data[zero_mask],adj_slopes[zero_mask].T[zero_mask].T)
-    # prediction2a = np.divide(prediction2a,weights[zero_mask]) + intercepts[index][zero_mask]
-    #
-    # print "Prediction 2 A"
-    # print pearsonr(prediction2a, true_values[zero_mask])
-    #
-    # fold_setting = 5
-    #
-    # folds = list(itertools.combinations(range(fold_setting),int(fold_setting-1)))
-    #
-    # integer_mask = np.random.randint(fold_setting, size=data.shape[0])
-    #
-    # for fold in folds:
-    #
-    #     fold_mask = np.zeros(data.shape[0],dtype=bool)
-    #
-    #     for element in fold:
-    #         fold_mask = np.logical_or(fold_mask,integer_mask == element)
-    #
-    #     final_mask = np.logical_and(fold_mask, zero_mask)
-    #
-    #     unadjusted = np.dot(data[final_mask],adj_slopes[final_mask][:,np.logical_not(final_mask)])
-    #
-    #     final = np.divide(unadjusted,weights[np.logical_not(final_mask)]) + intercepts[index][np.logical_not(final_mask)]
-    #
-    #     print "Prediction 3"
-    #     print pearsonr(final,true_values[np.logical_not(final_mask)])
-    #     print np.sum(final_mask)
-    # return prediction2, zero_mask
+    correlation = np.power(correlation,10)
 
-    return prediction1
+    centered = data - means
+    unweighted_centered = np.multiply(np.tile(centered,(slopes.shape[0],1)).T,slopes)
+    weighted_centered = np.multiply(unweighted_centered,correlation)
+
+    centered_prediction = np.divide(np.sum(weighted_centered,axis=0),np.sum(np.abs(correlation),axis=0))
+
+    prediction2 = centered_prediction + means
+
+    print pearsonr(prediction2, means)
+    print pearsonr(prediction2, true_values)
+
+    print "Centering"
+    print np.sum(centered)
+    print np.sum(centered_prediction)
+    print pearsonr(centered,centered_prediction)
+
+    return prediction2
 
 def main():
 
@@ -207,9 +156,12 @@ def main():
 
     # partial = partial_correlation(counts)
 
+
+
     for pick in np.random.randint(counts.shape[0], size=5):
 
         print pick
+
 
         predict(counts[pick,:],counts[pick,:], slopes, intercepts, means, correlations)
 
