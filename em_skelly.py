bf487a62974aa2911ff706ec9ce7884567c270cc
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-08-07 20:00:34 -0400

    transfer

--- em_skelly.py
+++ em_skelly.py
@@ -8,24 +8,24 @@
 
 import numpy as np
 
-from linear_regression import *
+import stripped_regression
+
 from sklearn.decomposition import PCA
 import scipy.spatial.distance as spt
 
+def predict_gene_counts(cell_matrix, iteration, distances, linear_model):
+
+    predicted_counts = np.zeros(cell_matrix.shape)
+
+    for i, cell in enumerate(cell_matrix):
+        predicted_counts[i] = linear_model.predict_cell(cell)
+
+    # distance_weights =
+
 def main():
 
-    counts = np.load(sys.argv[1])
+    counts = np.load(prefix+"counts")
 
-    # counts = pre.scale(counts)
+    linear_model = stripped_regression.stripped_regression(counts, prefix = prefix)
 
-    slopes, intercepts, means, correlations = linear_regression(counts)
-
-    # partial = partial_correlation(counts)
-
-
-
-    for pick in np.random.randint(counts.shape[0], size=5):
-
-        print pick
-
-        predict(counts[pick,:],counts[pick,:], slopes, intercepts, means, correlations)
+    linear_model.test()
