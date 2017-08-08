c4d35c63692ceae1f46776f094a79db5c116386c
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-08-07 20:31:03 -0400

    transfer

--- em_skelly.py
+++ em_skelly.py
@@ -35,7 +35,7 @@ def main():
 
     print "Counts loaded:"
 
-    print counts.shape
+    # print counts.shape
 
     linear_model = stripped_regression.stripped_regression(counts, prefix = prefix)
 
