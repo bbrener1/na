7563b0f972bb8549ef19f9e253f4c8a563f1eaad
Boris M Brenerman <boris@tvmac.hwcampus.jhu.edu>
2017-09-17 15:53:39 -0400

    transfer

--- figures.py
+++ figures.py
@@ -55,58 +55,77 @@ header = np.load('header_backup.npy')
 #     plt.title(header[pick],size=6)
 #     plt.hist(counts[:,pick], bins=21,log=True)
 # plt.savefig("figures/gene_histogram_gigaplex.png",dpi=300)
+#
+# cfig = plt.figure("raw_expression_heatmap")
+# ax1 = cfig.add_axes((.1,0,.7,1))
+# ax2 = cfig.add_axes((.85,.3,.06,.4))
+# im = ax1.imshow(counts, cmap='hot')
+# ax1.set_xlabel("Genes")
+# ax1.set_ylabel("Cells")
+# ax1.set_title("Log2 expression values (Unordered)")
+# cfig.colorbar(mappable=im,cax=ax2)
+# plt.savefig('figures/raw_expression_heatmap', dpi=600)
+#
 
-cfig = plt.figure("raw_expression_heatmap")
-ax1 = cfig.add_axes((.1,0,.7,1))
-ax2 = cfig.add_axes((.85,.3,.06,.4))
-im = ax1.imshow(counts, cmap='hot')
-ax1.set_xlabel("Genes")
-ax1.set_ylabel("Cells")
-ax1.set_title("Log2 expression values (Unordered)")
-cfig.colorbar(mappable=im,cax=ax2)
-plt.savefig('figures/raw_expression_heatmap', dpi=600)
+#
+#
+# gene_linked = hrc.linkage(counts.T, method='average', metric='correlation')
+# gene_dendrogram = hrc.dendrogram(gene_linked,no_plot=True)
+#
+# cell_linked = hrc.linkage(counts, method='average', metric='cosine')
+# clusterization = hrc.fcluster(cell_linked, criterion='inconsistent',t=.5,)
+# cell_dendrogram = hrc.dendrogram(cell_linked,no_plot=True)
+#
+# fig = plt.figure("doubly_clustered", figsize=(8,4))
+# ax1 = fig.add_axes([.09,.1,.2,.6])
+# # display_dendrogram = hrc.dendrogram(cell_linked, p=3, truncate_mode='level',orientation='left',show_contracted=True,ax=ax1)
+# with plt.rc_context({'lines.linewidth':0.1}):
+#     display_dendrogram = hrc.dendrogram(cell_linked,orientation='left',ax=ax1)
+# # ax1.set_xlim(left=1.0,right=.75)
+# # ax1.set_xscale('log')
+#
+# ax2 = fig.add_axes([.3,.71,.55,.2])
+# # display_dendrogram = hrc.dendrogram(gene_linked, p=3, truncate_mode='level',ax=ax2)
+# with plt.rc_context({'lines.linewidth':0.1}):
+#     display_dendrogram = hrc.dendrogram(gene_linked, ax=ax2)
+# # ax2.set_ylim(top=1,bottom=.75)
+# # ax2.set_yscale('log')
+#
+# print counts.shape
+# print len(cell_dendrogram['leaves'])
+# print len(gene_dendrogram['leaves'])
+#
+#
+# ax3 = fig.add_axes([.3,.1,.55,.6])
+#
+# sorted_singly = counts[np.flip(cell_dendrogram['leaves'],0)]
+# sorted_doubly = sorted_singly.T[gene_dendrogram['leaves']].T
+# # sorted_doubly = np.concatenate((sorted_doubly.T,np.ones((sorted_doubly.T.shape[0],1))*-10), axis=1)
+# # sorted_doubly = np.concatenate((sorted_doubly,np.ones((sorted_doubly.shape[0],1))*10), axis=1)
+# im = ax3.imshow(sorted_doubly, cmap='hot', aspect='auto')
+# # plt.title("Residual Expression of Genes In Cells, Clustered Hierarchically")
+# # plt.xlabel("Genes")
+# # plt.ylabel("Cells")
+# ax4 = fig.add_axes([.85,.1,.05,.6])
+# # ax4.set_ylim(bottom=-10,top=10)
+# fig.colorbar(mappable=im, fraction=.99, ax=ax4)
+# np.save("clustered_counts",sorted_doubly)
+# np.save("clustered_header",header[gene_dendrogram['leaves']])
+# np.save("gene_clustering_indecies", gene_dendrogram['leaves'])
+# np.save("cell_clustering_indecies", cell_dendrogram['leaves'])
+# plt.savefig("figures/doubly_clustered_raw_genes.png", dpi=800)
 
-
-
-
-gene_linked = hrc.linkage(counts.T, method='average', metric='correlation')
-gene_dendrogram = hrc.dendrogram(gene_linked,no_plot=True)
-
-cell_linked = hrc.linkage(counts, method='average', metric='cosine')
-clusterization = hrc.fcluster(cell_linked, criterion='inconsistent',t=.5,)
-cell_dendrogram = hrc.dendrogram(cell_linked,no_plot=True)
-
-fig = plt.figure("doubly_clustered", figsize=(8,4))
-ax1 = fig.add_axes([.09,.1,.2,.6])
-# display_dendrogram = hrc.dendrogram(cell_linked, p=3, truncate_mode='level',orientation='left',show_contracted=True,ax=ax1)
-with plt.rc_context({'lines.linewidth':0.1}):
-    display_dendrogram = hrc.dendrogram(cell_linked,orientation='left',ax=ax1)
-# ax1.set_xlim(left=1.0,right=.75)
-# ax1.set_xscale('log')
-
-ax2 = fig.add_axes([.3,.71,.55,.2])
-# display_dendrogram = hrc.dendrogram(gene_linked, p=3, truncate_mode='level',ax=ax2)
-with plt.rc_context({'lines.linewidth':0.1}):
-    display_dendrogram = hrc.dendrogram(gene_linked, ax=ax2)
-# ax2.set_ylim(top=1,bottom=.75)
-# ax2.set_yscale('log')
-
-print counts.shape
-print len(cell_dendrogram['leaves'])
-print len(gene_dendrogram['leaves'])
-
-
-ax3 = fig.add_axes([.3,.1,.55,.6])
-
-sorted_singly = counts[np.flip(cell_dendrogram['leaves'],0)]
-sorted_doubly = sorted_singly.T[gene_dendrogram['leaves']].T
-# sorted_doubly = np.concatenate((sorted_doubly.T,np.ones((sorted_doubly.T.shape[0],1))*-10), axis=1)
-# sorted_doubly = np.concatenate((sorted_doubly,np.ones((sorted_doubly.shape[0],1))*10), axis=1)
-im = ax3.imshow(sorted_doubly, cmap='hot', aspect='auto')
-# plt.title("Residual Expression of Genes In Cells, Clustered Hierarchically")
-# plt.xlabel("Genes")
-# plt.ylabel("Cells")
-ax4 = fig.add_axes([.85,.1,.05,.6])
-# ax4.set_ylim(bottom=-10,top=10)
-fig.colorbar(mappable=im, fraction=.99, ax=ax4)
-plt.savefig("figures/doubly_clustered_raw_genes.png", dpi=800)
+fig = plt.figure("gene_scatter_gigaplex")
+plt.suptitle("Set of scatter plots of expression values for randomly chosen gene pairs")
+plt.xlabel("Log2 gene expression values")
+plt.ylabel("Frequency")
+for i, pick1 in enumerate(np.random.randint(counts.shape[1], size=20)):
+    for j, pick2 in enumerate(np.random.randint(counts.shape[1], size=20)):
+        plt.subplot(4,5,i+1)
+        plt.xlabel(header[pick1],size=6)
+        plt.ylabel(header[pick2],size=6)
+        plt.xticks(np.arange(0,14,2),size=3)
+        plt.yticks(np.arange(2,10,2),size=3)
+        plt.scatter(counts[:,pick1],counts[:,pick2],marker='x',s=.1,alpha=.1,c='b')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.savefig("figures/gene_scatter_gigaplex.png",dpi=500)
