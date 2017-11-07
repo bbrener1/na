09ae44a9d20c340be7720fff9a935ff3e506bcec
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-11-07 16:47:33 -0500

    transfer

--- figures.py
+++ figures.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import scipy.special
+from scipy.stats import linregress
 
 import numpy as np
 
@@ -14,13 +15,19 @@ from sklearn.cluster import AgglomerativeClustering
 
 
 counts = np.load('counts.npy')
+deviation_matrix = np.load('numeric_deviation_matrix.npy')
+dropout_deviation_matrix = np.load('dropout_deviation_matrix.npy')
 header = np.load('header_backup.npy')
+cell_identities = np.load('cell_identity.npy')
 correlations = np.corrcoef(counts.T)
 correlations[np.identity(correlations.shape[0],dtype=bool)]=0
 
 clustered_counts = np.load('clustered_counts.npy')
 clustered_header = np.load('clustered_header.npy')
+# clustered_cell_labels = np.load('clustered_cells.npy')
 
+cell_clustering_indecies = np.load('cell_clustering_indecies.npy')
+gene_clustering_indecies = np.load('gene_clustering_indecies.npy')
 
 # plt.figure("counts_frequency")
 # plt.hist(counts.ravel(),bins=101,log=True)
@@ -152,7 +159,9 @@ clustered_header = np.load('clustered_header.npy')
 # plt.xlabel("Log2 gene expression values")
 # plt.ylabel("Frequency")
 # x, y = np.where(np.abs(correlations) > .5)
+# correlate_picks = []
 # for i, pick in enumerate(np.random.randint(x.shape[0], size=20)):
+#     correlate_picks.append(pick)
 #     plt.subplot(4,5,i+1)
 #     plt.xlabel(header[x[pick]],size=6)
 #     plt.ylabel(header[y[pick]],size=6)
@@ -160,10 +169,25 @@ clustered_header = np.load('clustered_header.npy')
 #     plt.yticks(np.arange(2,10,2),size=3)
 #     plt.scatter(counts[:,x[pick]],counts[:,y[pick]],marker='x',s=.1,alpha=.1,c='b')
 # plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-# print x.shape
+# # print x.shape
 # plt.savefig("figures/correlated_scatter_gigaplex.png",dpi=500)
 #
-# plt.figure("random_correlates")
+# plt.suptitle("Fitted linear regressions of correlated genes")
+# # plt.xlabel("Log2 gene expression values")
+# # plt.ylabel("Frequency")
+# # x, y = np.where(np.abs(correlations) > .5)
+# for i, pick in enumerate(correlate_picks):
+#     slope, intercept, rvalue, pvalue, std_err = linregress(counts[:,x[pick]], y=counts[:,y[pick]])
+#     plt.subplot(4,5,i+1)
+#     plt.plot(counts[:,x[pick]], intercept + counts[:,x[pick]]*slope, 'r', label = str(np.around(rvalue, decimals=3)))
+#     plt.legend(fontsize=5)
+# # plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+# # print x.shape
+# plt.savefig("figures/linear_fit_gigaplex.png",dpi=500)
+
+
+#
+# plt.figure("random_med_correlates")
 # plt.suptitle("Set of scatter plots for correlated gene pairs, .1<r<.5")
 # plt.xlabel("Log2 gene expression values")
 # plt.ylabel("Frequency")
@@ -179,8 +203,93 @@ clustered_header = np.load('clustered_header.npy')
 # print x.shape
 # plt.savefig("figures/correlated_scatter_gigaplex(intermediate).png",dpi=500)
 
-high_correlation_pairings_output = open("figures/high_correlations_pairings_output.txt", mode='w')
-x, y = np.where(np.abs(correlations) > .5)
-correlations_formatted_list = map(lambda z: (header[z[0]],header[z[1]],correlations[z[0],z[1]]), zip(x,y))
-for pairing in sorted(correlations_formatted_list, key=lambda x: x[2]):
-    high_correlation_pairings_output.write("\t".join(map(lambda x: str(x), pairing)) + "\n")
+# high_correlation_pairings_output = open("figures/high_correlations_pairings_output.txt", mode='w')
+# x, y = np.where(np.abs(correlations) > .5)
+# correlations_formatted_list = map(lambda z: (header[z[0]],header[z[1]],correlations[z[0],z[1]]), zip(x,y))
+# for pairing in sorted(correlations_formatted_list, key=lambda x: x[2]):
+#     high_correlation_pairings_output.write("\t".join(map(lambda x: str(x), pairing)) + "\n")
+
+# gene_cluster_at_3500_output = open("figures/gene_cluster_3500.txt", mode='w')
+# cell_cluster_at_700_output = open("figures/cell_cluster_700.txt", mode='w')
+#
+# for i, gene in enumerate(clustered_header[3600:3700]):
+#     gene_cluster_at_3500_output.write(str(gene) + '\t' + str(clustered_counts[620:840,3600+i].mean(axis = 0)) + "\n")
+#
+# cell_clustering_indecies = np.load('cell_clustering_indecies.npy')
+#
+# clustered_cells = cell_identities[cell_clustering_indecies]
+#
+# for cell in clustered_cells[620:840]:
+#     cell_cluster_at_700_output.write('\t'.join(map(lambda x: str(x),cell)) + '\n')
+#
+# print clustered_cells[620:840].sum(axis=0)
+# print clustered_cells.sum(axis=0)
+#
+#
+# padded_deviation = np.zeros((deviation_matrix.shape[0],deviation_matrix.shape[1]+np.sum(np.logical_not(dropout_deviation_matrix))))
+# shifted_index = 0
+# for i in range(len(dropout_deviation_matrix)):
+#     if dropout_deviation_matrix[i]:
+#         padded_deviation[:,i] = deviation_matrix[:,i-shifted_index]
+#     else:
+#         padded_deviation[:,i] = np.zeros(padded_deviation.shape[0])
+#         shifted_index += 1
+#
+#
+# print padded_deviation.shape
+# rearranged_deviation = padded_deviation[cell_clustering_indecies]
+# rearranged_deviation = rearranged_deviation.T[gene_clustering_indecies].T
+# rearranged_deviation = np.concatenate((rearranged_deviation,np.ones((rearranged_deviation.shape[0],1))*-7.5), axis=1)
+# rearranged_deviation = np.concatenate((rearranged_deviation,np.ones((rearranged_deviation.shape[0],1))*7.5), axis=1)
+#
+#
+# plt.figure('cluster_deviation')
+# image = plt.imshow(rearranged_deviation,cmap='seismic')
+# plt.colorbar(image)
+# plt.savefig('figures/neighbor_deviation.png', dpi=700)
+
+fig = plt.figure('clustered_residuals', figsize=(8,4))
+
+cell_linked = hrc.linkage(deviation_matrix, method='average', metric='cosine')
+clusterization = hrc.fcluster(cell_linked, criterion='inconsistent',t=.5,)
+cell_dendrogram = hrc.dendrogram(cell_linked,no_plot=True)
+gene_linked = hrc.linkage(deviation_matrix.T, method='average', metric='cosine')
+gene_dendrogram = hrc.dendrogram(gene_linked,no_plot=True)
+
+
+ax1 = fig.add_axes([.05,.1,.15,.6])
+# display_dendrogram = hrc.dendrogram(cell_linked, p=3, truncate_mode='level',orientation='left',show_contracted=True,ax=ax1)
+with plt.rc_context({'lines.linewidth':0.1}):
+    display_dendrogram = hrc.dendrogram(cell_linked,orientation='left',ax=ax1)
+ax1.set_xlim(left=1.0,right=.75)
+ax1.set_xscale('log')
+
+ax2 = fig.add_axes([.3,.71,.55,.2])
+# display_dendrogram = hrc.dendrogram(gene_linked, p=3, truncate_mode='level',ax=ax2)
+with plt.rc_context({'lines.linewidth':0.1}):
+    display_dendrogram = hrc.dendrogram(gene_linked, ax=ax2)
+ax2.set_ylim(top=1,bottom=.75)
+ax2.set_yscale('log')
+
+print len(cell_dendrogram['leaves'])
+print len(gene_dendrogram['leaves'])
+
+
+ax3 = fig.add_axes([.25,.1,.6,.6])
+
+clustered_residuals = deviation_matrix.T[gene_dendrogram['leaves']]
+clustered_residuals = clustered_residuals.T[cell_dendrogram['leaves']].T
+clustered_residuals = np.concatenate((clustered_residuals.T,np.ones((clustered_residuals.T.shape[0],1))*-7), axis=1)
+clustered_residuals = np.concatenate((clustered_residuals,np.ones((clustered_residuals.shape[0],1))*7), axis=1)
+im = ax3.imshow(clustered_residuals, cmap='seismic', aspect='auto')
+# plt.title("Residual Expression of Genes In Cells, Clustered Hierarchically")
+# plt.xlabel("Genes")
+# plt.ylabel("Cells")
+ax4 = fig.add_axes([.85,.1,.05,.6])
+# ax4.set_ylim(bottom=-10,top=10)
+fig.colorbar(mappable=im, fraction=.99, ax=ax4)
+plt.savefig("figures/clustered_residuals.png",dpi=700)
+
+np.save('residual_gene_clusters', gene_dendrogram['leaves'])
+np.save('residual_cell_clusters', cell_dendrogram['leaves'])
+np.save('clustered_residuals', clustered_residuals)
